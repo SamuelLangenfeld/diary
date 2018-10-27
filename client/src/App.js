73a583fe1c69f7831db0cccc708c295e484f1e2f
SamuelLangenfeld <samlangenfeld@gmail.com>
2018-10-27 11:49:18 -0400

    add save function to entry page

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,16 @@ import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 
-const PaperStyle = { marginBottom: "1rem" };
+const fetch = window.fetch;
+
+const paperStyle = { marginBottom: "1rem" };
+
+const headlineStyle = { marginTop: "1rem", marginBottom: "1rem" };
 
 const PaperBlock = props => {
   return (
     <Grid item xs={12} spacing={0}>
-      <Paper style={PaperStyle} {...props} />
+      <Paper style={paperStyle} {...props} />
     </Grid>
   );
 };
@@ -30,55 +34,83 @@ class App extends Component {
     //   .catch(err => console.log(err));
   }
 
+  constructor(props) {
+    super(props);
+    this.state = { title: "", body: "" };
+  }
+
   changeTitle = e => {
+    const title = e.target.value;
     this.setState(state => {
       return {
         ...state,
-        title: e.target.value
+        title
       };
     });
   };
 
   changeBody = e => {
+    const body = e.target.value;
     this.setState(state => {
       return {
         ...state,
-        body: e.target.value
+        body
       };
     });
+  };
+
+  saveEntry = () => {
+    const { title, body } = this.state;
+    var headers = new Headers();
+    headers.append("content-type", "application/json");
+    const options = {
+      method: "POST",
+      body: JSON.stringify({ title, body }),
+      headers
+    };
+    fetch("/api/entries", options)
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
   };
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Grid container direction={"row"} justify={"center"} md={12}>
           <Grid item container direction={"row"} xs={12} md={6}>
-            <PaperBlock>
-              <Typography variant="headline" component="h3">
+            <Grid item xs={12}>
+              <Typography
+                style={headlineStyle}
+                variant="headline"
+                component="h3"
+              >
                 Your Greatest Achievo
               </Typography>
-              <Typography>Body Typography</Typography>
+            </Grid>
+            <PaperBlock>
               <TextField
                 multiline
-                placeholder={"Go Ahead"}
+                placeholder={"Title . . ."}
                 fullWidth
-                onchange={this.changeTitle}
+                name={"title"}
+                onChange={this.changeTitle}
               />
             </PaperBlock>
             <PaperBlock>
               <TextField
                 multiline
-                rows={10}
-                placeholder={"What's up?"}
+                rows={20}
+                placeholder={"What's up? . . ."}
                 fullWidth
-                onchange={this.changeBody}
+                name={"body"}
+                onChange={this.changeBody}
               />
             </PaperBlock>
-            <Button variant={"raised"} color={"primary"}>
+            <Button
+              variant={"contained"}
+              color={"primary"}
+              onClick={this.saveEntry}
+            >
               Save your shit
             </Button>
           </Grid>
